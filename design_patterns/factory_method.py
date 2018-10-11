fc5fdc49e58b76d6ceb48cf207b201114bb559ae
Christoph Theis <christoph_theis@outlook.com>
2018-10-11 17:48:12 +0200

    Implementation and test of factory method pattern

---
+++ design_patterns/factory_method.py
@@ -0,0 +1,52 @@
+"""
+Templates for the Factory Method design pattern
+
+Remark: Due to dynamic typing in Python we don't need an (abstract) Product class since we don't
+need to declare the type of products that are created by the Creator class
+"""
+
+
+class Product:
+    """
+    Sample product to be created by the Factory Method
+    """
+
+    def __init__(self, some_value):
+        """
+        Initializes the sample property of Product
+        @param some_value: sample property
+        @type some_value: str
+        """
+        self.some_property = some_value
+
+    def some_method(self):
+        """
+        Sample method of Product
+        """
+        pass
+
+
+class Creator:
+    """
+    Base class of concrete Creators
+    """
+
+    def factory_method(self):
+        """
+        abstract factory method
+        """
+        raise NotImplementedError('factory_method has to be implemented in a subclass')
+
+
+class ConcreteCreator(Creator):
+    """
+    Subclass of Creator that implements the factory_method
+    """
+
+    def factory_method(self):
+        """
+        implementation of factory method
+        @return: a product
+        @rtype: instance of Product
+        """
+        return Product('some_value')
---
+++ design_patterns/test/test_factory_method.py
@@ -0,0 +1,65 @@
+"""
+Unittests for the Factory Method Pattern
+
+@author: Dr. Christoph Theis
+@since: 11.10.2018
+@version: 0.1
+"""
+
+import unittest
+
+from design_patterns.factory_method import Product
+from design_patterns.factory_method import Creator
+from design_patterns.factory_method import ConcreteCreator
+
+
+class ProductTest(unittest.TestCase):
+    """
+    Unittest for the Product class
+    """
+
+    def test_product_defines_properties_and_methods(self):
+        """
+        tests that instance of Product with some dummy property and method can be created
+        """
+        product = Product('some_value')
+        self.assertEqual(product.some_property, 'some_value')
+        self.assertTrue(hasattr(product, 'some_method'))
+
+
+class CreatorTest(unittest.TestCase):
+    """
+    Unittest for the Creator class (containing) factory_method as abstract method
+    """
+
+    def test_creator_has_factory_method_but_doesnt_implement_it(self):
+        """
+        tests that Creator defines a factory_method but has no implementation
+        """
+        creator = Creator()
+        self.assertTrue(hasattr(creator, 'factory_method'))
+        self.assertRaises(NotImplementedError, creator.factory_method)
+
+
+class ConcreteCreatorTest(unittest.TestCase):
+    """
+    Unittest for ConcreteCreator class, i.e. a subclass of Creator that implements the factory_method
+    """
+
+    def test_is_subclass_of_creator(self):
+        """
+        tests that ConcreteCreator is a subclass of Creator
+        """
+        self.assertIsInstance(ConcreteCreator(), Creator)
+
+    def test_implements_factory_method(self):
+        """
+        tests that ConcreteCreator implements the factory_method that return a Product
+        """
+        concrete_creator = ConcreteCreator()
+        self.assertTrue(hasattr(concrete_creator, 'factory_method'))
+        self.assertIsInstance(concrete_creator.factory_method(), Product)
+
+
+if __name__ == '__main__':
+    unittest.main()
